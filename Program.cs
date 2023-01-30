51b680efaa27cdfcc020558b69441556be606434
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-01-30 20:25:38 +0530

    Single Player

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeLadder
+{
+   public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Snake.snakeladderProgram();
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Snake
+    {
+        public static void snakeladderProgram()
+        {
+            int position = 0;
+            Console.WriteLine("Start Positon Is :" + position);
+        }
+    }
+}
